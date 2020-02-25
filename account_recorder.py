d7654515072c8c0efcd123a02efd1c454ca903af
Evan Walters <evanatyourservice@gmail.com>
2020-02-25 12:57:53 -0800

    typo

--- account_recorder.py
+++ account_recorder.py
@@ -32,14 +32,16 @@ def get_account(client):
 
         account_write = {
             "measurement": "account",
-            "tags": {
-                "username": account["username"],
-            },
+            "tags": {"username": account["username"],},
             "fields": {
                 "collateral": account["collateral"],
                 "freeCollateral": account["freeCollateral"],
-                "percentUsedCollateral": (account["collateral"] - account["freeCollateral"]) / account["collateral"],
-                "percentFreeCollateral": account["freeCollateral"] / account["collateral"],
+                "percentUsedCollateral": (
+                    account["collateral"] - account["freeCollateral"]
+                )
+                / account["collateral"],
+                "percentFreeCollateral": account["freeCollateral"]
+                / account["collateral"],
                 "marginFraction": account["marginFraction"],
                 "openMarginFraction": account["openMarginFraction"],
                 "totalAccountValue": account["totalAccountValue"],
@@ -48,43 +50,55 @@ def get_account(client):
             },
             "time": t,
         }
-        account_write["fields"] = {k: float(v) for k, v in account_write["fields"].items() if v is not None}
+        account_write["fields"] = {
+            k: float(v) for k, v in account_write["fields"].items() if v is not None
+        }
         client.write_points([account_write])
 
         if positions:
-            positions_write = [{
-                "measurement": "positions",
-                "tags": {
-                    "future": p["future"],
-                    "side": p["side"],
-                },
-                "fields": {
-                    "collateralUsed": p["collateralUsed"],
-                    "cost": p["cost"],
-                    "entryPrice": p["entryPrice"],
-                    "estimatedLiquidationPrice": p["estimatedLiquidationPrice"],
-                    "netSize": p["netSize"],
-                    "openSize": p["openSize"],
-                    "realizedPnl": p["realizedPnl"],
-                    "size": p["size"],
-                    "unrealizedPnl": p["unrealizedPnl"],
-                },
-                "time": t,
-            } for p in positions]
-            for di in positions_write:
-                di["fields"] = {k: float(v) for k, v in di["fields"].items() if v is not None}
+            positions_write = [
+                {
+                    "measurement": "positions",
+                    "tags": {"future": p["future"], "side": p["side"],},
+                    "fields": {
+                        "collateralUsed": p["collateralUsed"],
+                        "cost": p["cost"],
+                        "entryPrice": p["entryPrice"],
+                        "estimatedLiquidationPrice": p["estimatedLiquidationPrice"],
+                        "netSize": p["netSize"],
+                        "openSize": p["openSize"],
+                        "realizedPnl": p["realizedPnl"],
+                        "size": p["size"],
+                        "unrealizedPnl": p["unrealizedPnl"],
+                    },
+                    "time": t,
+                }
+                for p in positions
+            ]
+            for p in positions_write:
+                p["fields"] = {
+                    k: float(v) for k, v in p["fields"].items() if v is not None
+                }
             client.write_points(positions_write)
 
             all_positions_write = {
                 "measurement": "all_positions",
                 "fields": {
-                    "totalCollateralUsed": sum([p["collateralUsed"] for p in positions]),
+                    "totalCollateralUsed": sum(
+                        [p["collateralUsed"] for p in positions]
+                    ),
                     "totalCost": sum([p["cost"] for p in positions]),
                     "totalRealizedPnl": sum([p["realizedPnl"] for p in positions]),
+                    "totalUnrealizedPnl": sum([p["unrealizedPnl"] for p in positions]),
                 },
                 "time": t,
             }
-            client.write_points([positions_write])
+            all_positions_write["fields"] = {
+                k: float(v)
+                for k, v in all_positions_write["fields"].items()
+                if v is not None
+            }
+            client.write_points([all_positions_write])
 
 
 def get_balances(client):
@@ -97,18 +111,19 @@ def get_balances(client):
         t = datetime.utcnow().isoformat()
         balances = balances["info"]["result"]
 
-        balances_write = [{
-            "measurement": "balances",
-            "tags": {
-                "coin": c["coin"],
-            },
-            "fields": {
-                "free": float(c["free"]),
-                "total": float(c["total"]),
-                "usdValue": float(c["usdValue"]),
-            },
-            "time": t,
-        } for c in balances]
+        balances_write = [
+            {
+                "measurement": "balances",
+                "tags": {"coin": c["coin"],},
+                "fields": {
+                    "free": float(c["free"]),
+                    "total": float(c["total"]),
+                    "usdValue": float(c["usdValue"]),
+                },
+                "time": t,
+            }
+            for c in balances
+        ]
         client.write_points(balances_write)
 
 
@@ -119,7 +134,7 @@ def get_orders(client, first=False):
         since = int(time.time() - 120)
 
     try:
-        orders = Exchange.privateGetOrdersHistory(params={'start_time': since})
+        orders = Exchange.privateGetOrdersHistory(params={"start_time": since})
     except ccxt.BaseError as e:
         logger.error(f"Could not get order history with error: {e}")
         raise e
@@ -127,28 +142,33 @@ def get_orders(client, first=False):
         orders = orders["result"]
 
         if orders:
-            orders_write = [{
-                "measurement": "orders",
-                "tags": {
-                    "future": o["future"],
-                    "market": o["market"],
-                    "type": o["type"],
-                    "side": o["side"],
-                    "reduceOnly": o["reduceOnly"],
-                    "status": o["status"],
-                    "postOnly": o["postOnly"],
-                },
-                "fields": {
-                    "avgFillPrice": o["avgFillPrice"],
-                    "filledSize": o["filledSize"],
-                    "price": o["price"],
-                    "size": o["size"],
-                },
-                "time": o["createdAt"][:-6] + 'Z',
-            } for o in orders]
+            orders_write = [
+                {
+                    "measurement": "orders",
+                    "tags": {
+                        "future": o["future"],
+                        "market": o["market"],
+                        "type": o["type"],
+                        "side": o["side"],
+                        "reduceOnly": o["reduceOnly"],
+                        "status": o["status"],
+                        "postOnly": o["postOnly"],
+                    },
+                    "fields": {
+                        "avgFillPrice": o["avgFillPrice"],
+                        "filledSize": o["filledSize"],
+                        "price": o["price"],
+                        "size": o["size"],
+                    },
+                    "time": o["createdAt"][:-6] + "Z",
+                }
+                for o in orders
+            ]
             for o in orders_write:
                 o["tags"] = {k: str(v) for k, v in o["tags"].items() if v is not None}
-                o["fields"] = {k: float(v) for k, v in o["fields"].items() if v is not None}
+                o["fields"] = {
+                    k: float(v) for k, v in o["fields"].items() if v is not None
+                }
             client.write_points(orders_write)
 
 
@@ -159,7 +179,7 @@ def get_fills(client, first=False):
         since = int(time.time() - 120)
 
     try:
-        fills = Exchange.privateGetFills(params={'start_time': since})
+        fills = Exchange.privateGetFills(params={"start_time": since})
     except ccxt.BaseError as e:
         logger.error(f"Could not get fills history with error: {e}")
         raise e
@@ -167,33 +187,36 @@ def get_fills(client, first=False):
         fills = fills["result"]
 
         if fills:
-            fills_write = [{
-                "measurement": "fills",
-                "tags": {
-                    "future": f["future"],
-                    "market": f["market"],
-                    "type": f["type"],
-                    "liquidity": f["liquidity"],
-                    "side": f["side"],
-                },
-                "fields": {
-                    "fee": f["fee"],
-                    "feeRate": f["feeRate"],
-                    "price": f["price"],
-                    "size": f["size"],
-                },
-                "time": f["time"][:-6] + 'Z',
-            } for f in fills]
+            fills_write = [
+                {
+                    "measurement": "fills",
+                    "tags": {
+                        "future": f["future"],
+                        "market": f["market"],
+                        "type": f["type"],
+                        "liquidity": f["liquidity"],
+                        "side": f["side"],
+                    },
+                    "fields": {
+                        "fee": f["fee"],
+                        "feeRate": f["feeRate"],
+                        "price": f["price"],
+                        "size": f["size"],
+                    },
+                    "time": f["time"][:-6] + "Z",
+                }
+                for f in fills
+            ]
             for f in fills_write:
                 f["tags"] = {k: str(v) for k, v in f["tags"].items() if v is not None}
-                f["fields"] = {k: float(v) for k, v in f["fields"].items() if v is not None}
+                f["fields"] = {
+                    k: float(v) for k, v in f["fields"].items() if v is not None
+                }
             client.write_points(fills_write)
 
 
 def recorder():
-    client = InfluxDBClient(
-        host="localhost", port=8086, database="accountdb"
-    )
+    client = InfluxDBClient(host="localhost", port=8086, database="accountdb")
 
     first = True
 
