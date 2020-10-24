ffc54b152244e399cf8082aa3f092f3f4ea265bd
Evan Walters <evanatyourservice@gmail.com>
2020-10-24 12:54:53 -0400

    db name different from measurement

--- account_recorder.py
+++ account_recorder.py
@@ -216,7 +216,7 @@ def get_fills(client, first=False):
 
 
 def recorder():
-    client = InfluxDBClient(host="localhost", port=8086, database="account")
+    client = InfluxDBClient(host="localhost", port=8086, database="accountinfo")
 
     first = True
 
